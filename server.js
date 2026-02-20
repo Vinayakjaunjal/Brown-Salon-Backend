10584eae11bf4edf52f1f8dcf96911e3cfca4279
Vinayakjaunjal <157274693+Vinayakjaunjal@users.noreply.github.com>
2026-02-20 14:49:01 +0530

    Mobile Fix

--- server.js
+++ server.js
@@ -27,14 +27,9 @@ app.use(
       "https://brown-salon-frontend.vercel.app",
       "https://brownsalon.co.in",
     ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
 );
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
 
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
