96d5659351945f7f2b803028e86908e8d6810179
Vinayakjaunjal <157274693+Vinayakjaunjal@users.noreply.github.com>
2026-02-20 14:30:50 +0530

    Mobile Backend Issue

--- server.js
+++ server.js
@@ -21,7 +21,20 @@ const errorHandler = require("./middleware/errorHandler");
 const app = express();
 
 // MIDDLEWARE
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://brown-salon-frontend.vercel.app",
+      "https://brownsalon.co.in",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
