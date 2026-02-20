f66366a40ae90e8a53f30539ff94cbfe47f7476d
Vinayakjaunjal <157274693+Vinayakjaunjal@users.noreply.github.com>
2026-02-20 15:17:59 +0530

    Last fix to Both

--- server.js
+++ server.js
@@ -27,9 +27,14 @@ app.use(
       "https://brown-salon-frontend.vercel.app",
       "https://brownsalon.co.in",
     ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
 );
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
