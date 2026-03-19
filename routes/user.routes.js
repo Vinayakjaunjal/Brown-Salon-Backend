cf56a68a4d95f8d9b71403abf633884b980af3ac
Vinayakjaunjal <157274693+Vinayakjaunjal@users.noreply.github.com>
2026-03-19 10:31:54 +0530

    Fix User Profile

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,15 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const { getProfile, updateProfile } = require("../controllers/user.controller");
+const auth = require("../middleware/auth");
 
-router.get("/profile", (req, res) => {
-  res.json({
-    success: true,
-    data: {
-      name: "Vinayak Jaunjal",
-      email: "vinayak@test.com",
-      phone: "8208027615",
-    },
-  });
-});
+router.get("/profile", auth, getProfile);
+router.put("/profile", auth, updateProfile);
 
 module.exports = router;
