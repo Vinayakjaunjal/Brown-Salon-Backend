6a92583c05b266ef560e5c8d9d70dc5b93e37117
Vinayakjaunjal <157274693+Vinayakjaunjal@users.noreply.github.com>
2026-03-20 17:24:42 +0530

    Update Email API

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,15 +1,29 @@
 const SibApiV3Sdk = require("sib-api-v3-sdk");
-
 const client = SibApiV3Sdk.ApiClient.instance;
 client.authentications["api-key"].apiKey = process.env.BREVO_API_KEY;
 
 const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 
 module.exports = async ({ to, subject, html }) => {
-  await apiInstance.sendTransacEmail({
-    sender: { email: "brown.unisex.salon@gmail.com", name: "Brown Salon" },
-    to: [{ email: to }],
-    subject,
-    htmlContent: html,
-  });
+  try {
+    await apiInstance.sendTransacEmail({
+      sender: {
+        email: "brown.unisex.salon@gmail.com",
+        name: "Brown Salon",
+      },
+
+      to: [
+        {
+          email: to,
+        },
+      ],
+
+      subject: subject,
+      htmlContent: html,
+    });
+
+    console.log("EMAIL SENT:", to);
+  } catch (err) {
+    console.log("EMAIL ERROR:", err.message);
+  }
 };
