d8771b70a10c4c991c46a3b39d5df99494b0c329
Vinayakjaunjal <157274693+Vinayakjaunjal@users.noreply.github.com>
2026-03-23 11:29:33 +0530

    Fix Autocrop images

--- controllers/gallery.controller.js
+++ controllers/gallery.controller.js
@@ -1,18 +1,37 @@
 const Gallery = require("../models/Gallery");
+const sharp = require("sharp");
+const fs = require("fs");
+const path = require("path");
 
 exports.getGallery = async (req, res) => {
   res.json(await Gallery.find().sort({ order: 1 }));
 };
 
 exports.uploadImage = async (req, res) => {
-  const count = await Gallery.countDocuments();
+  try {
+    const count = await Gallery.countDocuments();
+    const filePath = path.join("uploads/gallery", req.file.filename);
+    const outputPath = path.join(
+      "uploads/gallery",
+      "optimized-" + req.file.filename,
+    );
 
-  await Gallery.create({
-    image: `/uploads/gallery/${req.file.filename}`,
-    order: count,
-  });
+    await sharp(filePath)
+      .resize(800, 600)
+      .jpeg({ quality: 70 })
+      .toFile(outputPath);
 
-  res.json({ success: true });
+    fs.unlinkSync(filePath);
+    await Gallery.create({
+      image: `/uploads/gallery/optimized-${req.file.filename}`,
+      order: count,
+    });
+
+    res.json({ success: true });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: err.message });
+  }
 };
 
 exports.deleteImage = async (req, res) => {
